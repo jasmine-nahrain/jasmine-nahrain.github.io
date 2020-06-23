a1e818ec0277f42f878fc2b947261bb541b119eb
jasmine-nahrain <ja5m1ne@hotmail.com>
2020-06-23 16:23:58 +1000

    Updates

---
+++ precache-manifest.6d206109b203869f1e8bf97b14693966.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f99f0cdb307167679a5899c7826062",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25730e48fc744bfd8ae9",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "aebfced887bb51b8ba2f",
+    "url": "/static/css/main.17dc1208.chunk.css"
+  },
+  {
+    "revision": "25730e48fc744bfd8ae9",
+    "url": "/static/js/2.d5f5679a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d5f5679a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aebfced887bb51b8ba2f",
+    "url": "/static/js/main.2ae243bc.chunk.js"
+  },
+  {
+    "revision": "42dded47c92ef7027de7",
+    "url": "/static/js/runtime-main.f2c5987a.js"
+  },
+  {
+    "revision": "327b48584f220c21ff001c59294daab2",
+    "url": "/static/media/email.327b4858.svg"
+  },
+  {
+    "revision": "3ae67d09a100e4f5a0f7bde93f6c94cf",
+    "url": "/static/media/github.3ae67d09.svg"
+  },
+  {
+    "revision": "014978cc7bda23398cb7a648550e4728",
+    "url": "/static/media/linkedin.014978cc.svg"
+  },
+  {
+    "revision": "13ccff9fc11dc3b572b4cc5fc2cca7b0",
+    "url": "/static/media/quantum.13ccff9f.PNG"
+  },
+  {
+    "revision": "2db6119778e7967a46e536a15441c492",
+    "url": "/static/media/quantum2.2db61197.PNG"
+  },
+  {
+    "revision": "561cdf9da5447807ba7a86b23fed32b2",
+    "url": "/static/media/resume.561cdf9d.pdf"
+  },
+  {
+    "revision": "3eb0b83e0b8d30ae1019b70e02e0aa90",
+    "url": "/static/media/tel.3eb0b83e.png"
+  },
+  {
+    "revision": "4e34f1059719646af9dcfab9e8e1c9ba",
+    "url": "/static/media/tel2.4e34f105.png"
+  }
+]);
