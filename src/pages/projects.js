fc70f3012553c62c8c0d8045a38f7c4824515b76
jasmine-nahrain <ja5m1ne@hotmail.com>
2020-06-26 10:16:39 +1000

    hail mary

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import {BrowserRouter} from 'react-router-dom';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import tel from '../images/tel.png';
