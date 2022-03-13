738ecece2388474abaa4db460e2c0dd6789ec599
ja5m1ne@hotmail.com <ja5m1ne@hotmail.com>
2022-03-13 13:57:48 +1100

    sabs fault

--- src/pages/projects2.js
+++ src/pages/projects2.js
@@ -10,6 +10,9 @@ import projects from './projects.json';
 import github from '../images/github.svg';
 import {FiGithub} from 'react-icons/fi';
 
+var colours = ['rgb(230,230,230)', '#383838']
+var fontColours = ['#383838', 'rgb(230,230,230)']
+
 export default class Projects extends Component {
 
   render() {
@@ -17,39 +20,30 @@ export default class Projects extends Component {
     return (
       <BrowserRouter>
       <h1>Academic Projects</h1>
+      <div class="project">
         {projects.map((info, i) =>
           <div class="project-container" key={i}>
-            <div className="project-content">
-              <p className="project-overline">{info.year}</p>
+            <div className="project-content" style={{'grid-column': info.id%2==0 ? '2' : '1', 'grid-row': info.id-1+''}}>
+              <p className="project-overline">{info.date}</p>
               <h2 className="project-title">{info.title}</h2>
               <div className="project-description">
               <p>{info.description}</p>
               </div>
 
-              {info.techstack.length && (
-                <ul className="project-tech-list">
-                  {info.techstack.map((tech, i) => (
-                    <li key={i}>{tech}</li>
-                  ))}
-                </ul>
-              )}
-
               <div className="project-links">
-                {info.github && (
-                  <a href={info.github} aria-label="GitHub Link">
+                {info.linkedin && (
+                  <a href={info.github} target="_blank" aria-label="Github Link">
                     <FiGithub src={github} color="white" size="24px"/>
                   </a>
                 )}
               </div>
               </div>
-
-              <div className="project-image">
-              <a href={info.github}>
-                <img alt={info.title} className="img" src={info.image}/>
-              </a>
+              <div className="project-image" style={{'grid-column': info.id%2==0 ? '1' : '2', 'grid-row': info.id-1+''}}>
+                <img src={info.image} />
+              </div>
             </div>
-          </div>
         )}
+        </div>
       </BrowserRouter>
     );
   }
